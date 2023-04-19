ab73e0aec9ba1c1ed5a2128492915693a91c216a
sagiv bonali <sagivb13@gmail.com>
2023-04-19 15:07:50 +0300

    inital project to work with pages and layouts

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-layout>
+        <v-app-bar title="Application bar">
+            <v-app-bar-nav-icon @click="drawer = !drawer" />
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer">
+            <v-list>
+                <v-list-item title="Navigation drawer"></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-main style="min-height: 300px;">
+            <slot> </slot>
+        </v-main>
+    </v-layout>
+</template>
+<script setup lang="ts">
+const drawer = ref<boolean>();
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <v-card title="This is the title from index page."></v-card>
+</template>
