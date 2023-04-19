9ce310b6de35d6cec77850dce5871de3f0940e89
sagiv bonali <sagivb13@gmail.com>
2023-04-19 22:51:50 +0300

    Test for supabase

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    {{ data }}
+    <v-btn @click="u">Click!</v-btn>
+</template>
+<script setup>
+const client = useSupabaseClient();
+const { data } = await useAsyncData('note', async () => {
+    return await client.from('note').select('*')
+})
+const u = async () => {
+    console.log("A")
+    const a = await client.from('note').insert({ 'title': "Bela bela" })
+    console.log({ a })
+};
+</script>
