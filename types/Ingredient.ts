6b1b54efd2c03494fd4df1debeeec1826d7222e6
sagiv bonali <sagivb13@gmail.com>
2023-04-20 17:59:22 +0300

    Create interface for Ingredient

--- types/Ingredient.ts
+++ types/Ingredient.ts
@@ -1,5 +1,24 @@
 declare interface Ingredient {
-  quantity: string;
+  createdAt: string;
+  id: number;
   name: string;
   type: string;
+  tags: string[];
+}
+
+declare interface InsertIngredient {
+  createdAt?: string;
+  id?: number;
+  name?: string 
+  type: string;
+  tags?: string[];
+}
+
+
+declare interface UpdateIngredient {
+  createdAt?: string;
+  id: number;
+  name?: string;
+  type?: string;
+  tags?: string[];
 }
