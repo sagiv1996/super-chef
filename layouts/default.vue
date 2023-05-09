58932252721e6fbc3e6efd47432e8d6224f1e646
sagiv bonali <sagivb13@gmail.com>
2023-05-09 14:03:11 +0300

    add append to  navigation-drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,13 @@
                 <v-list>
                     <v-list-item title="Navigation drawer"></v-list-item>
                 </v-list>
-                <v-switch v-model="darkMode" @change="toggleTheme" />
+
+                <template v-slot:append>
+                    <div class="pa-2">
+                        <v-switch v-model="darkMode" @change="toggleTheme" true-icon="mdi-weather-night"
+                            label="Select theme" false-icon="mdi-brightness-6" />
+                    </div>
+                </template>
             </v-navigation-drawer>
 
             <v-main style="min-height: 300px;">
