bebcf54727978782a3d52f3de43fc6d364cddd39
sagiv bonali <sagivb13@gmail.com>
2023-05-10 12:08:53 +0300

    Add support multi device

--- components/shoppingList/addItemToShoppingList.vue
+++ components/shoppingList/addItemToShoppingList.vue
@@ -1,16 +1,16 @@
 <template>
     <v-form ref="form" @submit.prevent="handleSubmit">
         <v-row>
-            <v-col cols="9">
+            <v-col cols="12" sm="9">
                 <search-ingredient @changeItem="(val: string) => ingredient = val" />
             </v-col>
-            <v-col cols="2">
+            <v-col cols="9" sm="2">
                 <v-text-field type="number" label="Amount" v-model.number="amount" prepend-inner-icon="mdi-minus"
                     append-inner-icon="mdi-plus" @click:append-inner="amount++" @click:prepend-inner="decrement" min="1"
                     :rules="[rules.isRequired, rules.isGreaterThan0]">
                 </v-text-field>
             </v-col>
-            <v-col cols="1">
+            <v-col cols="3" sm="1">
                 <v-btn type="submit">submit</v-btn>
             </v-col>
         </v-row>
