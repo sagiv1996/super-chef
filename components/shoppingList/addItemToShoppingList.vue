7cd293089c21a125f3c752e2ce2b8f703d5d63c8
sagiv bonali <sagivb13@gmail.com>
2023-05-10 12:30:40 +0300

    Change size for button

--- components/shoppingList/addItemToShoppingList.vue
+++ components/shoppingList/addItemToShoppingList.vue
@@ -4,14 +4,14 @@
             <v-col cols="12" sm="9">
                 <search-ingredient @changeItem="(val: string) => ingredient = val" />
             </v-col>
-            <v-col cols="9" sm="2">
+            <v-col cols="8" sm="2">
                 <v-text-field type="number" label="Amount" v-model.number="amount" prepend-inner-icon="mdi-minus"
                     append-inner-icon="mdi-plus" @click:append-inner="amount++" @click:prepend-inner="decrement" min="1"
                     :rules="[rules.isRequired, rules.isGreaterThan0]">
                 </v-text-field>
             </v-col>
-            <v-col cols="3" sm="1">
-                <v-btn type="submit">submit</v-btn>
+            <v-col cols="4" sm="1">
+                <v-btn type="submit" size="x-large">submit</v-btn>
             </v-col>
         </v-row>
     </v-form>
