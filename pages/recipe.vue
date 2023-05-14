445765ba944915505a688cb5a5ce1de40414930c
sagiv bonali <sagivb13@gmail.com>
2023-05-14 11:02:14 +0300

    Create recipe page

---
+++ pages/recipe.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-row>
+        <v-col v-for="recipe in recipes">
+            <recipe-card :recipe="recipe" />
+        </v-col>
+    </v-row>
+</template>
+
+<script setup lang="ts">
+
+const config = useRuntimeConfig();
+const { BASE_URL } = config.public
+
+const { data: recipes, pending, error, refresh } = await useAsyncData<Recipe[]>(
+    () => $fetch(
+        'recipe',
+        {
+            baseURL: BASE_URL
+        }
+    )
+)
+</script>
