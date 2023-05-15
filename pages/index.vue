9e7826404c9020760c41af688fca0c5d348bc4f7
sagiv bonali <sagivb13@gmail.com>
2023-05-15 08:57:06 +0300

    Update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,6 @@
 <template>
     <v-card title="title" />
 </template>
-
-<!-- 
-<template>
-    <template v-if="pending"><v-card-title title="Loading...." /></template>
-    <template v-else-if="error"><v-card-title title="Error" /></template>
-    <v-row v-else-if="recipes?.body">
-        <v-col v-for="(recipe) in recipes.body">
-            <recipe-card :recipe="recipe" />
-        </v-col>
-    </v-row>
-</template>
 <script setup lang="ts">
-const { data: recipes, pending, error } = await useAsyncData(
-    () => queryContent<LoadRecipe>('/recipes').findOne()
-) -->
 
-<!-- </script> -->
+</script>
